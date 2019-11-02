35201cc61dd9f10a902f3f18ea65766448b96008
avielkar <avraham.elkar@gmail.com>
2019-11-02 13:58:35 +0200

    Add init source fies

---
+++ Assets/Manager/TCPReceiver.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Assets.Manager
+{
+    public class TCPReceiver : MonoBehaviour
+    {
+        Socket SeverSocket = null;
+        Thread Socket_Thread = null;
+        bool Socket_Thread_Flag = false;
+
+        SceneManager SceneManager;
+
+        //string[] stringSeparators = new string[] { "*TOUCHEND*", "*MOUSEDELTA*", "*Tapped*", "*DoubleTapped*" };
+
+        void Awake()
+        {
+            Socket_Thread = new Thread(Dowrk);
+            Socket_Thread_Flag = true;
+            Socket_Thread.Start();
+            SceneManager = SceneManager.Instance;
+        }
+
+        private void Dowrk()
+        {
+            //receivedMSG = new string[10];
+            SeverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 5000);
+            SeverSocket.Bind(ipep);
+            SeverSocket.Listen(10);
+
+            Debug.Log("Socket Standby....");
+            Socket client = SeverSocket.Accept(); //client
+            Debug.Log("Socket Connected.");
+
+            IPEndPoint clientep = (IPEndPoint)client.RemoteEndPoint;
+            NetworkStream recvStm = new NetworkStream(client);
+            //tick = 0;
+
+            while (Socket_Thread_Flag)
+            {
+                byte[] receiveBuffer = new byte[1024 * 80];
+                try
+                {
+                    if (recvStm.Read(receiveBuffer, 0, receiveBuffer.Length) == 0)
+                    {
+                        // when disconnected , wait for new connection.
+                        client.Close();
+                        SeverSocket.Close();
+
+                        SeverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                        ipep = new IPEndPoint(IPAddress.Any, 1000);
+                        SeverSocket.Bind(ipep);
+                        SeverSocket.Listen(10);
+                        Debug.Log("Socket Standby....");
+                        client = SeverSocket.Accept(); //client
+                        Debug.Log("Socket Connected.");
+
+                        clientep = (IPEndPoint)client.RemoteEndPoint;
+                        recvStm = new NetworkStream(client);
+                    }
+                    else
+                    {
+                        //string Test = Encoding.Default.GetString(receiveBuffer);
+                        //SceneManager.ReadStringMessageFromSocket(Test);
+
+                        //MessageToUnity msg = new MessageToUnity();
+                        //msg.Data = receiveBuffer;
+                        byte[] msg = receiveBuffer;
+                        //avi:SceneManager.OnMessageReceived(msg);
+                    }
+                }
+
+                catch (Exception)
+                {
+                    Socket_Thread_Flag = false;
+                    client.Close();
+                    SeverSocket.Close();
+                    continue;
+                }
+
+            }
+
+        }
+
+        void OnApplicationQuit()
+        {
+            try
+            {
+                Socket_Thread_Flag = false;
+                Socket_Thread.Abort();
+                SeverSocket.Close();
+                Debug.Log("Unity was shut down. Closed all Socket connections.");
+            }
+
+            catch
+            {
+                Debug.Log("Error when finished...");
+            }
+        }
+    }
+}
---
+++ Assets/Scenes/Starfield Skybox/Scripts/CameraMovement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float speed = 50.0f;
+    public float sensitivity = 0.25f;
+    public bool inverted = false;
+
+    private Vector3 lastMouse = new Vector3(225, 225, 225);
+
+    public bool smooth = true;
+    public float acceleration = 0.01f;
+    private float actSpeed = 0.0f;
+    private Vector3 lastDir = new Vector3();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //TODO: the speed needs to be determined by moog?
+        moveSpeed = 50.0f;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {     
+        lastMouse = Input.mousePosition - lastMouse;
+        if (!inverted) lastMouse.y = -lastMouse.y;
+        lastMouse *= sensitivity;
+        lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.y, transform.eulerAngles.y + lastMouse.x, 0);
+        transform.eulerAngles = lastMouse;
+
+        lastMouse = Input.mousePosition;
+
+
+        //TODO: Read type of input and determine what code to use.
+        //TODO: if it is joystick/xbox - use the following code:
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime,
+                            0f,
+                            moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        //TODO: if it is uncontrolled by the subject, create the given trajectory movement
+        /*Vector3 dir = new Vector3();
+
+        if (Input.GetKey(KeyCode.W)) dir.z += 1.0f;
+        if (Input.GetKey(KeyCode.S)) dir.z -= 1.0f;
+        if (Input.GetKey(KeyCode.A)) dir.x -= 1.0f;
+        if (Input.GetKey(KeyCode.D)) dir.x += 1.0f;
+        dir.Normalize();
+
+        if(dir != Vector3.zero)
+        {
+            if (actSpeed < 1)
+                actSpeed += acceleration * Time.deltaTime * 40;
+            else
+                actSpeed = 1.0f;
+            lastDir = dir;
+        }
+        else
+        {
+            if (actSpeed > 0)
+                actSpeed -= acceleration * Time.deltaTime * 20;
+            else
+                actSpeed = 0.0f;
+        }
+
+        if (smooth)
+            transform.Translate(lastDir * actSpeed * speed * Time.deltaTime);
+        else
+            transform.Translate(dir * speed * Time.deltaTime);*/
+    }
+}
